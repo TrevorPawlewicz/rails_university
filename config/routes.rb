8df9d2922ae54386c9514dc25d8049a9f1a1ad33
Trev <trevor.pawlewicz@gmail.com>
2020-01-03 14:21:29 -0500

    started building Students controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
     get 'courses/new', to: 'courses#new'
 
     get 'about', to: 'pages#about'
+
+    resources :students, except: [:destroy]
+
+    # resources produces:
+    #     students GET   /students(.:format)          students#index
+    #              POST  /students(.:format)          students#create
+    #  new_student GET   /students/new(.:format)      students#new
+    # edit_student GET   /students/:id/edit(.:format) students#edit
+    #      student GET   /students/:id(.:format)      students#show
+    #              PATCH /students/:id(.:format)      students#update
+    #              PUT   /students/:id(.:format)      students#update
 end
